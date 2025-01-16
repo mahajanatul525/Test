b50728e698f1a00162bbe7497b9f888b6099c850
Atul <mahajanatul525@gmail.com>
2025-01-16 10:05:52 +0530

    renamed class

--- src/StreamExample.java
+++ src/StreamExample.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Sample {
+public class StreamExample {
 
     public static void main(String[] args) {
 
